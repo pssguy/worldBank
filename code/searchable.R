0b9dd3b0785ee1c5d681db30dd0e0c177401497e
pssguy <agcur@rogers.com>
2016-04-28 08:24:02 -0700

    Code for passing across indicator Id but hiding from selection  table

--- code/searchable.R
+++ code/searchable.R
@@ -9,7 +9,7 @@ print(input$searchValue) # eg poverty
 searchValue <- input$searchValue
 
 
-df<- wbsearch(pattern = searchValue, fields = c("indicator", "indicatorDesc"),
+df<- wbsearch(pattern = searchValue, fields = c("indicator", "indicatorDesc","indicatorID"),
               extra = TRUE) # have not got latest cache
 # info=list(searchValue=searchValue)
 # return(info)
@@ -22,10 +22,12 @@ output$tableChoice <- DT::renderDataTable({
  #           extra = TRUE) # have not got latest cache
  # 
  searchData() %>%
-   select(indicator,indicatorDesc) %>% 
-                         DT::datatable(class='compact stripe hover row-border order-column',rownames=TRUE,selection='single',options= list(paging = TRUE, searching = TRUE,info=FALSE))
+   select(indicatorID,indicator,indicatorDesc) %>% 
+                         DT::datatable(class='compact stripe hover row-border order-column',rownames=TRUE,selection='single',options= list(paging = TRUE, searching = TRUE,info=FALSE,
+                                                                                                                                           columnDefs = list(list(visible=FALSE, targets=list(1)))))
 })
 
+#options=list(columnDefs = list(list(visible=FALSE, targets=columns2hide)))
 
 ## now get data for selected table
 
@@ -35,6 +37,8 @@ wbData <- reactive({
   
   
   s = as.integer(input$tableChoice_rows_selected)
+  
+  id <- searchData()[s,]$indicatorID
   #   print(input$hthTable_selected) #1 so it is row
   #   
   #   s = input$hthTable_selected
